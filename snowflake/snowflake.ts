dacef38eee97228dadff490ad9249aaaa449250a
Karol Koś <kosiyyu@gmail.com>
2024-07-04 11:37:21 +0200

    Create class, optimise performance, add simple test

--- snowflake/snowflake.ts
+++ snowflake/snowflake.ts
@@ -1,16 +1,57 @@
+const MAX_SEQUENCE = (1n << 12n) - 1n;
+
 function ensureLength(arg: bigint, bitLength: bigint): bigint {
   return arg & ((1n << bitLength) - 1n);
 }
 
-function generate(datacenterId: bigint, machineId: bigint, sequence: bigint): bigint {
-  return (ensureLength(BigInt(Date.now()), 41n) << 22n) |
-    (ensureLength(datacenterId, 5n) << 17n) |
-    (ensureLength(machineId, 5n) << 12n) |
-    ensureLength(sequence, 12n);
+class Generator {
+  private timestamp: bigint;
+  private oldTimestamp: bigint;
+  private sequence: bigint;
+  private datacenterId: bigint;
+  private machineId: bigint;
+
+  constructor(datacenterId: bigint, machineId: bigint) {
+    this.timestamp = BigInt(Date.now());
+    this.oldTimestamp = -1n;
+    this.sequence = 0n;
+    this.datacenterId = datacenterId;
+    this.machineId = machineId;
+  }
+
+  public next(): bigint {
+    if(this.sequence === MAX_SEQUENCE) {
+      while(this.timestamp === this.oldTimestamp) {
+        this.timestamp = BigInt(Date.now());
+      }
+      this.oldTimestamp = this.timestamp;
+      this.sequence = 0n;
+    }
+
+    this.sequence = (this.sequence + 1n) & MAX_SEQUENCE;
+
+    return (ensureLength(this.timestamp, 41n) << 22n) |
+    (ensureLength(this.datacenterId, 5n) << 17n) |
+    (ensureLength(this.machineId, 5n) << 12n) |
+    ensureLength(this.sequence, 12n);
+  }
 }
 
-const snowflakeId = generate(1n, 1n, 1n);
+function testUniqnes(timeoutInMs: number): boolean {
+  const start = Date.now();
+  const prev = -1n;
+  const generator = new Generator(1n, 1n);
 
-console.log(snowflakeId.toString(16));
-console.log(snowflakeId.toString(10));
-console.log(snowflakeId.toString(2));
+  while(true) {
+    if(prev === generator.next()) {
+      return false;
+    }
+    if(Date.now() - start >= timeoutInMs) {
+      break;
+    }
+  }
+  return true;
+}
+
+const testResult = testUniqnes(1000);
+console.log(testResult);
