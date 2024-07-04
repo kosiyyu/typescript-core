0349db2cf8b7aef5c805c1654cd5759e06671178
Karol Koś <kosiyyu@gmail.com>
2024-07-04 02:32:36 +0200

    Add bitmasking

--- snowflake/snowflake.ts
+++ snowflake/snowflake.ts
@@ -1,34 +1,19 @@
 let timestamp = BigInt(Date.now());
-let sequence = 1n;
-let datacenterId = 1n;
-let machineId = 1n;
+let sequence = 223n;
+let datacenterId = 23n;
+let machineId = 45n;
 
-function getBinaryLength(arg: bigint): number {
-  return (arg).toString(2).length;
+function ensureLength(arg: bigint, bitLength: bigint): bigint {
+  return arg & ((1n << bitLength) - 1n);
 }
 
-function moveLength(arg: bigint, bitLength: number): bigint {
- return BigInt(bitLength - getBinaryLength(arg));
-}
+timestamp = ensureLength(timestamp, 41n);
+datacenterId = ensureLength(datacenterId, 5n);
+machineId = ensureLength(machineId, 5n);
+sequence = ensureLength(sequence, 12n);
 
-// Ensure upper limit
-timestamp &= 0x1FFFFFFFFFFn; // <= 41 bits
-sequence &= 0xFFFn;          // <=  12 bits
-datacenterId &= 0x1Fn;       // <= 5 bits
-machineId &= 0x1Fn;          // <= 5 bits
+const snowflakeId = (timestamp << 22n) | (datacenterId << 17n) | (machineId << 12n) | sequence;
 
-let snowflakeId = (timestamp << 22n);
-
-sequence = sequence << moveLength(sequence, 22);
-snowflakeId = snowflakeId | sequence;
-
-datacenterId = datacenterId << moveLength(datacenterId, 10);
-snowflakeId = snowflakeId | datacenterId;
-
-machineId = machineId << moveLength(machineId, 5);
-snowflakeId = snowflakeId | machineId;
-
-snowflakeId = snowflakeId << 1n;
-
-console.log((snowflakeId).toString(2));
-console.log((snowflakeId).toString());
+console.log(snowflakeId.toString(16));
+console.log(snowflakeId.toString(10));
+console.log(snowflakeId.toString(2));
