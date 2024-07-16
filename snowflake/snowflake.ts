2e1299eeb70ecc9b31eab25c23701c9b1a27730d
Karol Koś <kosiyyu@gmail.com>
2024-07-16 23:49:58 +0200

    Add proper tests

--- snowflake/snowflake.ts
+++ snowflake/snowflake.ts
@@ -1,10 +1,10 @@
-const MAX_SEQUENCE = (1n << 12n) - 1n;
+export const MAX_SEQUENCE = (1n << 12n) - 1n;
 
 function ensureLength(arg: bigint, bitLength: bigint): bigint {
   return arg & ((1n << bitLength) - 1n);
 }
 
-class Generator {
+export class Generator {
   private timestamp: bigint;
   private oldTimestamp: bigint;
   private sequence: bigint;
@@ -36,22 +36,3 @@ class Generator {
       ensureLength(this.sequence, 12n);
   }
 }
-
-function testUniqnes(timeoutInMs: number): boolean {
-  const start = Date.now();
-  const prev = -1n;
-  const generator = new Generator(1n, 1n);
-
-  while (true) {
-    if (prev === generator.next()) {
-      return false;
-    }
-    if (Date.now() - start >= timeoutInMs) {
-      break;
-    }
-  }
-  return true;
-}
-
-const testResult = testUniqnes(1000);
-console.log(testResult);
