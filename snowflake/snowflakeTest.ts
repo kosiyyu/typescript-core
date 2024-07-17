fc3dbcb6239827dd7bd90676026ef2d4c4f8ea83
Karol Koś <kosiyyu@gmail.com>
2024-07-17 12:02:20 +0200

    Cleanup

--- snowflake/snowflakeTest.ts
+++ snowflake/snowflakeTest.ts
@@ -7,28 +7,28 @@ Deno.test("Generator - should handle sequence overflow", () => {
   for (let i = 0; i <= Number(MAX_SEQUENCE); i++) {
     generator.next();
   }
-  
+
   const id1 = generator.next();
   const id2 = generator.next();
-  
+
   assert(id1 !== id2, "IDs should be different after sequence overflow");
 });
 
 Deno.test("Generator - should use correct bit lengths", () => {
   const generator = new Generator(1n, 1n);
   const id = generator.next();
-  
+
   // Check timestamp (41 bits)
   assert(id >> 22n < 1n << 41n, "Timestamp should be less than 2^41");
-  
+
   // Check datacenter ID (5 bits)
   assertEquals((id >> 17n) & 0b11111n, 1n, "Datacenter ID should be 1");
-  
+
   // Check machine ID (5 bits)
   assertEquals((id >> 12n) & 0b11111n, 1n, "Machine ID should be 1");
-  
+
   // Check sequence (12 bits)
-   assert((id & 0b111111111111n) < 1n << 12n, "Sequence should be less than 2^12");
+  assert((id & 0b111111111111n) < 1n << 12n, "Sequence should be less than 2^12");
 });
 
 Deno.test("Generator - should generate monotonically increasing IDs", () => {
@@ -45,7 +45,7 @@ Deno.test("Generator - should generate unique IDs (note - it can take a few seco
 
   for (let i = 0n; i < numIds; i++) {
     const id = generator.next();
-    if(!ids.has(id)) ids.add(id);
+    if (!ids.has(id)) ids.add(id);
   }
 
   assertEquals(BigInt(ids.size), numIds, `Should have generated ${numIds} unique IDs`);
